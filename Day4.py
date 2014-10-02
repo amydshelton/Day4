7f208adae8138e58ffc3fd3856c0dfa23f966452
Hackbright Student <info@hackbrightacademy.com>
2014-10-02 11:59:09 -0700

    second exercise study drills

--- Day4.py
+++ Day4.py
@@ -1,15 +1,20 @@
-i = 0 
-numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def whileLoop(max):
+    i = 0 
+    numbers = []
+    while i < max:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "Numbers now:", numbers
-    print "At the bottom i is %d" % i
+        i = i + 1
+        print "Numbers now:", numbers
+        print "At the bottom i is %d" % i
 
-print "The numbers: "
+        
+    print "The numbers: "
 
-for num in numbers:
-    print num
+    for num in numbers:
+        print num
+    # return "hello"
+
+whileLoop(6)
